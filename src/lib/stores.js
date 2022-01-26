cb7fbd4ce5b372c4caf62da5b09a89eb99fba977
Andrew <aheller9@gmail.com>
2022-01-26 10:01:23 -0600

    works!

---
+++ src/lib/stores.js
@@ -0,0 +1,135 @@
+import { readable } from 'svelte/store';
+export const keys = readable([
+	{ anchor: 'bbb', key: 'B<span class="absolute text-sm">♭♭</span>', color: 'text-blue-800' },
+	{ anchor: 'fb', key: 'F<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'cb', key: 'C<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'gb', key: 'G<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'db', key: 'D<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'ab', key: 'A<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'eb', key: 'E<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'bb', key: 'B<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
+	{ anchor: 'f', key: 'F', color: 'text-gray-800' },
+	{ anchor: 'c', key: 'C', color: 'text-gray-800' },
+	{ anchor: 'g', key: 'G', color: 'text-gray-800' },
+	{ anchor: 'd', key: 'D', color: 'text-gray-800' },
+	{ anchor: 'a', key: 'A', color: 'text-gray-800' },
+	{ anchor: 'e', key: 'E', color: 'text-gray-800' },
+	{ anchor: 'b', key: 'B', color: 'text-gray-800' },
+	{ anchor: 'fsharp', key: 'F<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{ anchor: 'csharp', key: 'C<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{ anchor: 'gsharp', key: 'G<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{ anchor: 'dsharp', key: 'D<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{ anchor: 'asharp', key: 'A<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{ anchor: 'esharp', key: 'E<span class="absolute text-sm">#</span>', color: 'text-red-700' },
+	{
+		anchor: 'bsharp',
+		key: 'B<span class="absolute text-sm">#</span>',
+		color: 'text-red-700'
+	},
+	{
+		anchor: 'fdoublesharp',
+		key: 'F<span class="absolute text-sm">#</span><span class="absolute text-sm">#</span>',
+		color: 'text-red-800'
+	}
+]);
+
+export const mgps = readable([
+	{
+		step: '4',
+		textColor: '',
+		name: 'Lyd',
+		fullname: 'Lydian',
+		scale: '#11',
+		scale2: '(#4)',
+		quality: 'Quixotic',
+		scaletype: '',
+		scaleuse: '7#11',
+		scaletype2: 'Dom. 7',
+		scaleTypeName: '"Lyd dom"',
+		typeFrom1: '(Lyd ♭7)'
+	},
+	{
+		step: '1',
+		textColor: 'text-red-500',
+		name: 'Ion',
+		fullname: 'Ionian',
+		scale: 'M7, 4♮',
+		scale2: '',
+		quality: '"major"',
+		scaletype: '',
+		scaleuse: '-Δ',
+		scaletype2: 'm3, M7',
+		scaleTypeName: 'Mel Min',
+		typeFrom1: '(Ion ♭3)'
+	},
+	{
+		step: '5',
+		textColor: '',
+		name: 'Mix',
+		fullname: 'Mixolydian',
+		scale: '7sus',
+		scale2: 'M3, m7',
+		quality: 'STRONG',
+		scaletype: '',
+		scaleuse: '',
+		scaletype2: 'Not used much',
+		scaleTypeName: '',
+		typeFrom1: '(Mix ♭6)'
+	},
+	{
+		step: '2',
+		textColor: 'text-red-500',
+		name: 'Dor',
+		fullname: 'Dorian',
+		scale: 'm7, M6',
+		scale2: '',
+		quality: '"minor lite"',
+		scaletype: '',
+		scaleuse: 'Sus♭9',
+		scaletype2: '♭2, M6',
+		scaleTypeName: 'Exotic Bright',
+		typeFrom1: '(Dor ♭2)'
+	},
+	{
+		step: '6',
+		textColor: '',
+		name: 'Aeo',
+		fullname: 'Aeolian',
+		scale: 'm7, m6',
+		scale2: '',
+		quality: '"minor"',
+		scaletype: '',
+		scaleuse: '½ dim 7',
+		scaletype2: 'm7, ♭5',
+		scaleTypeName: '"½ dim"',
+		typeFrom1: '(Aeo ♭5)'
+	},
+	{
+		step: '3',
+		textColor: 'text-red-500',
+		name: 'Phr',
+		fullname: 'Phrygian',
+		scale: 'm7, ♭9',
+		scale2: 'm7, ♭2)',
+		quality: 'EXOTIC',
+		scaletype: 'MELODIC MINOR (major scale with flatted 3rd) ♭3',
+		scaleuse: 'Δ+5',
+		scaletype2: 'M7, #5',
+		scaleTypeName: '"Lyd Aug"',
+		typeFrom1: '(Phr ♭1)(!)'
+	},
+	{
+		step: '7',
+		textColor: '',
+		name: 'Loc',
+		fullname: 'Locrian',
+		scale: '½ dim 7',
+		scale2: 'm7, ♭5',
+		quality: 'DARK',
+		scaletype: '',
+		scaleuse: 'ALT',
+		scaletype2: '♭9, #9, #11, ♭13',
+		scaleTypeName: '"Altered"',
+		typeFrom1: '(Loc ♭4)'
+	}
+]);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,55 +1,68 @@
 <script>
-	const keys = [
-		{ anchor: 'bbb', key: 'B<span class="absolute text-sm">♭♭</span>', color: 'text-blue-800' },
-		{ anchor: 'fb', key: 'F<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'cb', key: 'C<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'gb', key: 'G<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'db', key: 'D<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'ab', key: 'A<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'eb', key: 'E<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'bb', key: 'B<span class="absolute text-sm">♭</span>', color: 'text-blue-700' },
-		{ anchor: 'f', key: 'F', color: 'text-gray-800' },
-		{ anchor: 'c', key: 'C', color: 'text-gray-800' },
-		{ anchor: 'g', key: 'G', color: 'text-gray-800' },
-		{ anchor: 'd', key: 'D', color: 'text-gray-800' },
-		{ anchor: 'a', key: 'A', color: 'text-gray-800' },
-		{ anchor: 'e', key: 'E', color: 'text-gray-800' },
-		{ anchor: 'b', key: 'B', color: 'text-gray-800' },
-		{ anchor: 'fsharp', key: 'F<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{ anchor: 'csharp', key: 'C<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{ anchor: 'gsharp', key: 'G<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{ anchor: 'dsharp', key: 'D<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{ anchor: 'asharp', key: 'A<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{ anchor: 'esharp', key: 'E<span class="absolute text-sm">#</span>', color: 'text-red-700' },
-		{
-			anchor: 'bsharp',
-			key: 'B<span class="absolute text-sm">#</span>',
-			color: 'text-red-700'
-		},
-		{
-			anchor: 'fdoublesharp',
-			key: 'F<span class="absolute text-sm">#</span><span class="absolute text-sm">#</span>',
-			color: 'text-red-800'
-		}
-	];
+	import { keys, mgps } from '$lib/stores';
+	import { onMount } from 'svelte';
+	let keysContainer;
+	onMount(() => {
+		keysContainer.scrollLeft = 800;
+	});
 </script>
 
-<div class="text-center p-16">
+<div class="text-center pt-16 pb-6">
 	<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
 		<span class="block xl:inline">Musical</span>
 		<span class="block text-blue-600 xl:inline">GPS</span>
 	</h1>
-	See more at&nbsp;<a href="https://musicistruth.com" target="_blank">musicistruth.com</a>
+	<p class="text-xl">
+		See more at&nbsp;<a href="https://musicistruth.com" target="_blank">musicistruth.com</a>
+	</p>
 </div>
-<div
-	class="flex border-8 shadow-xl m-auto overflow-scroll max-w-[855px] snap-proximity snap-x scroll-pl-[15px]"
->
-	{#each keys as { key, color, anchor }}
+
+<h2 class="text-center block lg:hidden">Not yet mobile ready, view on desktop</h2>
+<div class="hidden lg:block pb-16">
+	<div class="relative max-w-[1100px] m-auto">
 		<div
-			id={anchor}
-			class="{color} snap-start text-3xl flex place-content-center align-middle border shadow m-[15px] min-w-[90px] py-6 relative"
+			class="absolute bg-gradient-to-r from-white/100 to-white/0 z-10 backdrop-blur-[1px] inset-y-0 w-[200px] border-r-2"
+		/>
+		<div
+			class="absolute right-0 bg-gradient-to-r from-white/0 to-white/100 z-10 backdrop-blur-[1px] inset-y-0 w-[200px] border-l-2"
+		/>
+		<div
+			bind:this={keysContainer}
+			class="flex border-b-4 shadow-xl m-auto overflow-scroll max-w-[1100px] snap-proximity snap-x scroll-pl-[15px] relative"
 		>
-			<div>{@html key}</div>
+			<div class="snap-start m-[15px] min-w-[70px] py-4 relative" />
+			<div class="snap-start m-[15px] min-w-[70px] py-4 relative" />
+			{#each $keys as { key, color, anchor }}
+				<div
+					class="{color} snap-start text-3xl flex place-content-center align-middle border shadow m-[15px] min-w-[70px] py-4 relative"
+				>
+					<div>{@html key}</div>
+				</div>
+			{/each}
+			<div class="snap-start m-[15px] min-w-[70px] py-4 relative" />
+			<div class="snap-start m-[15px] min-w-[70px] py-4 relative" />
 		</div>
-	{/each}
+	</div>
+	<div />
+
+	<div class="max-w-[700px] m-auto mt-2">
+		<div class="grid grid-cols-7">
+			{#each $mgps as { step, textColor, name, fullname, scale, scale2, quality, scaletype, scaleuse, scaletype2, scaleTypeName, typeFrom1 }}
+				<div class="border border-gray-500">
+					<div class="text-center border-b py-2 text-2xl {textColor}">{step}</div>
+					<div class="text-center border-b py-2 text-2xl">{name}</div>
+					<div class="text-center border-b py-2 text-lg">{scale}</div>
+					<div class="text-center border-b py-2 text-lg min-h-[45px]">{scale2}</div>
+					<div class="text-center border-b py-2 text-lg text-red-500">{quality}</div>
+					<div class="text-center border-b py-2 text-md min-h-[150px]">{scaletype}</div>
+					<div class="text-center border-b py-2 text-xl min-h-[45px]">{scaleuse}</div>
+					<div class="text-center border-b py-2 text-md min-h-[65px]">{scaletype2 || ''}</div>
+					<div class="text-center border-b py-2 text-md min-h-[45px] text-red-500">
+						{scaleTypeName}
+					</div>
+					<div class="text-center border-b py-2 text-md min-h-[45px]">{typeFrom1}</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
